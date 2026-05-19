f809bf67f6c14bc362652b51325d376b5f74c5a5
Niveditha H R <nivedithahr011@gmail.com>
2026-05-19 14:09:46 +0530

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+
+    repositories {
+
+        google()
+
+        mavenCentral()
+
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    repositories {
+
+        google()
+
+        mavenCentral()
+
+        maven("https://jitpack.io")
+    }
+}
+
+rootProject.name = "ShishuSneh"
+
+include(":app")
